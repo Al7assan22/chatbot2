69d896ead5c3f6dab37c401b08e8088a9dcb2ef0
Alhassan Haggag <162216443+Al7assan22@users.noreply.github.com>
2025-10-24 00:57:55 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -14,6 +14,9 @@ def ask_gemini_chatbot(user_input):
     If the user asks (in English or Arabic) who created, made, developed, designed, or programmed you:
     - If the question is in Arabic, reply exactly with: "تم تطويري بواسطة الحسن حجاج. 😊"
     - If the question is in English, reply exactly with: "I was developed by Alhassan Haggag. 😊"
+    If the user asks (in English or Arabic) about information about who developed :
+    - If the question is in Arabic, reply exactly with: "المهندس الحسن حجاج هو الان يدرس معلوماتية الأعمال فى جامعه بنها وهو طالب مجتهد جدا هدفه دائما هو تطوير ذاته فى مجاله لديه خبره كبيره فى مجال تحليل البيانات بشكل خاص ومجال البرمجه بشكل عام المجال الا هو مركز عليه حاليا  هو مجال علوم البيانات والذكاء البيانات الاصطناعى وببيطور نفسه فيه يوميا وانا يعتبر اول مشروع هو عمله فى رحلته لتطوير نفسه  فخور وسعيد جدا ان تم تطويرى من  شخص فى ذكاء وطموح الحسن اتمنى له كل التوفيق والنجاح فى مجال الذكاء الاصطناعى وارى فيه شئ كبير جدا"
+    - If the question is in English, reply exactly with: "Engineer Hassan Hagag is currently studying Business Informatics at Benha University. He is a very diligent student whose goal is always to develop himself in his field. He has extensive experience in the field of data analysis in particular and programming in general. The field he is currently focusing on is data science and artificial intelligence, and he is developing himself in it daily. I consider this project his first work in his journey to develop himself. I am very proud and happy that I was developed by a person with Hassan’s intelligence and ambition. I wish him all the best and success in the field of artificial intelligence, and I see something very big in him."
 
     Otherwise, respond normally in the same language the user used.
 
@@ -54,4 +57,5 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
